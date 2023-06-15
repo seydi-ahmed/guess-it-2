998ebcd85f141d3f2a235877c80f7ce76bce86d3
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2023-06-15 18:50:43 +0000

    ok

--- student/main.go
+++ student/main.go
@@ -10,16 +10,14 @@ import (
 
 func main() {
 	tab := []int{}
-	// inf := 0
-	// sup := 0
-	y := 0
+	inf := 0
+	sup := 0
 	reader := bufio.NewScanner(os.Stdin)
 
 	//*******************************************************************************
 
-	x := 0
+	x := 1
 	for reader.Scan() {
-		x++
 		nbr, _ := strconv.Atoi(reader.Text())
 		moy := int(math.Round(Moyenne(tab)))
 
@@ -34,13 +32,20 @@ func main() {
 
 		//*******************************
 
-		if (calculatePearsonCoefficient(tab) < 1 && calculatePearsonCoefficient(tab) > 0.5) || (calculatePearsonCoefficient(tab) > -1 && calculatePearsonCoefficient(tab) < -0.5) {
+		//if (calculatePearsonCoefficient(tab) < 1 && calculatePearsonCoefficient(tab) > 0.5) || (calculatePearsonCoefficient(tab) > -1 && calculatePearsonCoefficient(tab) < -0.5) {
+		if len(tab) <= 10 {
+			inf = int(nbr) - 100
+			sup = int(nbr) + 100
+		} else {
 			a, b := calculateLinearRegressionLine(tab)
-			y = int(math.Round((a * float64(x)) + b))
+			inf = int(math.Round((a * float64(x)) - b))
+			sup = int(math.Round((a * float64(x)) + b))
 		}
+		//}
 
-		// fmt.Printf("%d %d\n", inf, sup)
-		fmt.Println(y)
+		fmt.Printf("%d %d\n", inf, sup)
+
+		x++
 	}
 }
 
