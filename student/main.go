b3b0dc075977bc71518f95e4a313c40fb1fce958
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-10 08:18:25 +0000

    Final Push

--- student/main.go
+++ student/main.go
@@ -5,70 +5,79 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"sort"
 	"strconv"
 )
 
 func main() {
-	reader := bufio.NewScanner(os.Stdin)
-	var data []float64
-	for reader.Scan() {
-		num, err := strconv.ParseFloat(reader.Text(), 64)
-		if err != nil {
-			fmt.Println("Error: number is not valid")
-			return
+	var arr []float64
+	ysum := 0.0
+	xsum := 0.0
+	XYNum := 0.0
+	XXsum := 0.0
+	YYsum := 0.0
+	index := 0.0
+	avr := 0.0
+	sd := 0.0
+	numOne := 0.0
+	fileScanner := bufio.NewScanner(os.Stdin)
+	for fileScanner.Scan() {
+		num, e := strconv.ParseFloat(fileScanner.Text(), 64)
+		if e != nil {
+			fmt.Printf("%T \n %v", num, num)
 		}
-		data = append(data, num)
-		min, max := RangeGuess(data)
-		fmt.Println(int(math.Round(min)), int(math.Round(max)))
-	}
-}
+		if index == 0 {
+			numOne = num
+		}
+		xsum = xsum + index
+		XXsum = XXsum + (index * index)
+		if num < 100 || num > 200 {
+			arr = append(arr, ysum/(index+1))
+			ysum = ysum + ysum/(index+1)
+			XYNum = XYNum + (ysum/(index+1))*index
+			YYsum = YYsum + (ysum/(index+1))*(ysum/(index+1))
+		} else {
+			arr = append(arr, num)
+			ysum = ysum + num
+			XYNum = XYNum + (num * index)
+			YYsum = YYsum + (num * num)
+		}
+		index++
+		b := ((XYNum - ((ysum * xsum) / index)) / (XXsum - ((xsum * xsum) / index)))
+		a := (((ysum * XXsum) - (xsum * XYNum)) / ((index * XXsum) - (xsum * xsum)))
+		rrr := 0.0
 
-func RangeGuess(data []float64) (float64, float64) {
-	var min, max float64
-	slope, intercept := LinearRegression(data)
-	cov := PCC(data)
-	prediction := slope*float64(len(data)-1) + intercept
-	max = prediction + prediction*(1-cov)
-	min = prediction - prediction*(1-cov)
-	return min, max
-}
+		calVal1 := b*index + a + index
+		if index <= 3 {
+			calVal1 = arr[int(index)-1]
+		} else {
+			rrr = arr[int(index)-1] - arr[int(index)-2]
+		}
+		calVal := num + rrr
+		sort.Float64s(arr)
+		avr = math.Round(ysum / float64(len(arr)))
+		for j := 0; j < len(arr); j++ {
+			sd += math.Pow((float64(arr[j]) - avr), 2)
+		}
+		sd = math.Round(math.Sqrt(sd / float64(len(arr))))
+		used := 0.0
+		low := 0
+		up := 0
+		used = (calVal + calVal1) / 2
+		if numOne > 169 && numOne < 180 {
+			low = int(math.Round(used - sd))
+			up = int(math.Round(used + (sd)))
+		} else if numOne > 159.0 {
+			low = int(math.Round(used - 5))
+			up = int(math.Round(used + (6)))
+		} else if numOne > 120.0 {
+			low = int(math.Round(used - 1))
+			up = int(math.Round(used + (1)))
+		} else {
+			low = int(math.Round(calVal1 - 10))
+			up = int(math.Round(calVal1 + (9)))
+		}
 
-func LinearRegression(data []float64) (float64, float64) {
-	sumX := 0.00
-	sumX2 := 0.00
-	sumY := 0.00
-	sumXY := 0.00
-	sumSQR := 0.00
-	lngth := float64(len(data))
-	for i := 0; i < len(data); i++ {
-		sumX += float64(i + 1)
-		sumX2 += float64(i)
-		sumY += data[i]
-		sumSQR += float64((i + 1) * (i + 1))
-		sumXY += (float64(i)) * data[i]
+		fmt.Printf("%d %d\n", low, up)
 	}
-	slope := ((lngth * sumXY) - (sumX2 * sumY)) / ((lngth * sumSQR) - (sumX * sumX))
-	if math.IsNaN(slope) {
-		slope = 0
-	}
-	intercept := data[len(data)-1] - (slope * float64(len(data)))
-	return slope, intercept
-}
-
-func PCC(data []float64) float64 {
-	sumX := 0.00
-	sumY := 0.00
-	sumXY := 0.00
-	sumSQRX := 0.00
-	sumSQRY := 0.00
-	lngth := float64(len(data))
-	for i := 0; i < len(data); i++ {
-		sumX += float64(i + 1)
-		sumY += data[i]
-		sumSQRX += float64((i + 1) * (i + 1))
-		sumSQRY += data[i] * data[i]
-		sumXY += (float64(i + 1)) * data[i]
-	}
-	covariance := ((lngth * sumXY) - (sumX * sumY)) / math.Sqrt(((lngth*sumSQRX)-(sumX*sumX))*((lngth*sumSQRY)-(sumY*sumY)))
-	return covariance
 }
